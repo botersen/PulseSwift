a20044c28e2b57d6754676347bca7613d6548ee8
Brennen Otersen <bjoters@gmail.com>
2025-08-01 14:51:03 -0400

    🔄 Fix globe tilt direction: +23° for proper northern view
    - Changed from -23° to +23° tilt
    - Now correctly showcases US and Europe
    - Northern hemisphere properly prioritized
    - Better geographic orientation for rotation

--- PulseSwift/Architecture/UI/Views/GlobeView.swift
+++ PulseSwift/Architecture/UI/Views/GlobeView.swift
@@ -168,7 +168,7 @@ struct GlobeSceneView: UIViewRepresentable {
         
         // Rotate and tilt the Earth to prioritize northern hemisphere (US/Europe)
         // Y rotation for correct longitude orientation + X tilt for northern view
-        earthNode.eulerAngles = SCNVector3(-0.4, Float.pi, 0) // -23° tilt to show northern hemisphere
+        earthNode.eulerAngles = SCNVector3(0.4, Float.pi, 0) // +23° tilt to show northern hemisphere
         
         scene.rootNode.addChildNode(earthNode)
         
